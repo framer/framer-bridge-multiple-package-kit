bef4acf74772c0d8a1c4f4057caadc0bb7cefe63
Addison <addison@framer.com>
2019-07-05 13:16:15 +0200

    Initial Commit

---
+++ framer/colors.framerfx/code/canvas.tsx
@@ -0,0 +1,32 @@
+// WARNING: this file is auto generated, any changes will be lost
+import { createDesignComponent, CanvasStore } from "framer"
+const canvas = CanvasStore.shared(); // CANVAS_DATA;
+
+export const colors = Object.freeze({
+    /** #FFFFFF */
+    "paneBg": "var(--token-d89314e9-87a5-42a7-8a24-91155cb0bb68, rgb(255, 255, 255))",
+    /** #FDFDFD */
+    "paneBgDark": "var(--token-7caa5d96-62eb-4895-8ae3-b16376196f8d, rgb(253, 253, 253))",
+    /** rgba(0, 0, 0, 0.1) */
+    "paneBorder": "var(--token-f0f434be-3b3f-4b74-b785-9d12f246534c, rgba(0, 0, 0, 0.1))",
+    /** #7755FF */
+    "primary": "var(--token-175b5d14-a110-438b-8548-09e63aa4f1cb, rgb(119, 85, 255))",
+    /** #7745FF */
+    "primaryDark": "var(--token-edaa0e36-e0bf-48b1-afd8-835197a33e2b, rgb(119, 69, 255))",
+    /** #EEEEFF */
+    "primaryLight": "var(--token-738f03bb-306e-49bb-815b-4c53892fd379, rgb(238, 238, 255))",
+    /** #FF4444 */
+    "danger": "var(--token-f741c0c4-8452-4411-a0c1-812d35d7634d, rgb(255, 68, 68))",
+    /** #FF4444 */
+    "dangerDark": "var(--token-2fe4e6cf-d810-47e5-b099-871297426c4a, rgb(255, 68, 68))",
+    /** #FFDDDD */
+    "dangerLight": "var(--token-98de536c-fd7f-4ff3-8435-93780d26702a, rgb(255, 221, 221))",
+    /** #222222 */
+    "body": "var(--token-f13b8352-fa99-4e48-bb9b-ed00fc9563fa, rgb(34, 34, 34))",
+    /** #444444 */
+    "bodyLight": "var(--token-ce8e7754-42aa-4b3a-b3b4-132411be02b2, rgb(68, 68, 68))",
+    /** #666666 */
+    "bodyLighter": "var(--token-3639a0fa-1c23-47da-99d4-e85a67592d47, rgb(102, 102, 102))",
+    /** #AAAAAA */
+    "bodyLightest": "var(--token-a27b6f31-a1ea-42d0-8a81-d6a43ab6ed55, rgb(170, 170, 170))",
+})
---
+++ path.js
@@ -0,0 +1,79 @@
+const { execSync } = require("child_process");
+const fs = require("fs");
+
+const EXAMPLE = `
+    Example package.json:
+
+    {
+      "name": "example-package",
+      "version": "1.1.0"
+      "paths": [
+        {
+          "componentPath": "path/to/component/Component.tsx",
+          "cmd": "npx framer-cli publish ./component.framerfx --yes"
+        }
+      ]
+    }
+
+    Where "cmd" is the command that will run if the regular expression under
+    "componentPath" matches any added, removed or modified file.
+`;
+
+const throwMissingChangesField = () =>
+  new Error(`Expected your package.json to have a non-empty array at the "paths" key:
+
+${EXAMPLE}
+`);
+
+const throwMissingcomponentPathField = () =>
+  new Error(`Expected your package.json's items to have a "componentPath" field:
+
+${EXAMPLE}
+`);
+
+const throwMissingCmdField = () =>
+  new Error(`Expected your package.json's items to have a "cmd" field:
+
+${EXAMPLE}
+`);
+
+const paths = execSync("git diff origin/master --name-status")
+  .toString()
+  .split("\n")
+  .map(line => line.match(/(\S+)\s+(\S+$)/))
+  .filter(l => !!l)
+  .map(parsed => {
+    return {
+      type: parsed[1],
+      file: parsed[2]
+    };
+  });
+
+const packageJson = JSON.parse(fs.readFileSync("package.json"));
+
+if (!packageJson["paths"] || packageJson["paths"].length === 0) {
+  throw throwMissingChangesField();
+}
+
+(packageJson["paths"] || []).forEach(changeSpec => {
+  const componentPath = changeSpec && changeSpec["componentPath"];
+  const cmd = changeSpec && changeSpec["cmd"];
+
+  if (!componentPath || typeof componentPath !== "string") {
+    throw throwMissingcomponentPathField();
+  }
+
+  if (!cmd || typeof cmd !== "string") {
+    throw throwMissingCmdField();
+  }
+
+  const compiledcomponentPath = new componentPathp(componentPath);
+  const changeDetected = paths.some(change => {
+    return compiledcomponentPath.test(change.file);
+  });
+
+  if (changeDetected) {
+    console.log("Change detected, running command:", cmd);
+    console.log(execSync(cmd).toString());
+  }
+});
